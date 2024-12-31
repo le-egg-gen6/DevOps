f12227d8fc94bd7df36438e9fd973ae4975f2bfe
nguyenle <lenn@falcongames.com>
2024-12-31 14:50:22 +0700

    fix: global config

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,16 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 4.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
 module "vpc" {
     source = "vpc"
 
